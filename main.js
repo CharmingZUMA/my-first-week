fa45f5827d34ac72d1d6ca305e9729ab2ce786d5
CharmingZUMA <adegbolamuaz@gmail.com>
2022-04-12 04:28:33 -0700

    second commit

---
+++ main.js
@@ -0,0 +1,22 @@
+//Task1
+console.log("Muaz Adegbola");
+
+//Task2
+console.log("2020");
+
+//Task3
+console.log(2020);
+
+//Task4
+console.log("Muaz","Adegbola",2020);
+
+//Task5
+console.log("Muaz");
+console.log("Adegbola");
+console.log(2020);
+
+//Task6
+console.log("Muaz"+"Adegbola"+2020);
+
+//Task7
+console.log("Muaz "+"Adegbola "+ 2020);
